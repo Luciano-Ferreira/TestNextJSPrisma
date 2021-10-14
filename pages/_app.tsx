949a51c7158c72c686ed5f862e4987cc333f6c53
Luciano Ferreira <luciano2023silva@gmail.com>
2021-10-14 16:34:59 -0300

    configure eslint w/ prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
-import { AppProps } from "next/app";
+import React from 'react';
+import { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return (
-    <Component {...pageProps} />
-  );
+    return (
+        <Component {...pageProps} />
+    );
 };
 
 export default App;
