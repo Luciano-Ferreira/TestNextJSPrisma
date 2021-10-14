3b8aa0b01c06323e8ca2183fc69b9de673d6a8ac
Luciano Ferreira <luciano2023silva@gmail.com>
2021-10-14 16:34:05 -0300

    fixing a global object in next

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare global {
+  namespace NodeJS {
+      interface Global {
+          prisma: any;
+      }
+  }
+}
+
+export { };
